fa12f104e387254052b673074d8be11c2f68bcc0
Steven Au <49203921+OwlAu@users.noreply.github.com>
2022-08-02 23:29:39 +0800

    Drafting overall page

---
+++ src/components/Home.jsx
@@ -0,0 +1,19 @@
+export const Home = () => {
+  return (
+    <div className="home">
+      <div className="homeDescription_text">
+        <div className="follow">
+          Follow
+          <div className="followDashline"></div>
+        </div>
+        <div className="dashline"></div>
+        <div className="jobTitle">Videographer and Director</div>
+        <div className="name">Mankey Sun Liew</div>
+        <div className="portfolioBtn" id="home">
+          My Portfolio
+        </div>
+      </div>
+      <img src="img/test.jpg" alt="" style={{ width: "100%" }} />
+    </div>
+  );
+};
